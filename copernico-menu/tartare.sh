803b5c7bd81241706d6c87d7117dd6a1825b4012
RobClap <robclap8@gmail.com>
2016-05-23 12:33:46 +0200

    Fixed a typo

--- copernico-menu/tartare.sh
+++ copernico-menu/tartare.sh
@@ -1,6 +1,6 @@
 wget 2>/dev/null -O /tmp/menu.pdf $(wget -O- 'http://www.copernicomilano.it/cafe-copernico/' 2>/dev/null | 
 grep  "SCOPRI IL MEN. DI OGGI" | 
-grep -Po 'href=" \K[^"]*')
+grep -Po 'href="\K[^"]*')
 pdftotext /tmp/menu.pdf - |
 grep -i "tartare" &&
 echo "C'è la tartare" ||

1549038d1a042b477e420257bd95e532d5677a06
RobClap <robclap8@gmail.com>
2016-06-09 23:59:36 +0200

    Removed useless stuff

--- satelliteyes/asabovesobelow.sh
+++ satelliteyes/asabovesobelow.sh
@@ -4,7 +4,7 @@
 
 [[ "$(uname -s)" =~ Linux ]] && 
 whereami () {
-curl "https://maps.googleapis.com/maps/api/browserlocation/json?browser=firefox&key=AIzaSyDBgL8fm9bD8RLShATOLI1xKmFcZ4ieMkM&sensor=true" --data-urlencode "$(nmcli -f SSID,BSSID,SIGNAL dev wifi list | sed -rn 's/(.*[^ ])[ ]*(..:..:..:..:..:..)  ([0-9]*) .*/\&wifi=mac:\2|ssid:\1|ss:\3/p' | tr -d '\n')" | grep -Po '"l(at|ng)" : \K[0-9.,]+' | tr -d '\n'
+curl "https://maps.googleapis.com/maps/api/browserlocation/json?browser=firefox&sensor=true" --data-urlencode "$(nmcli -f SSID,BSSID,SIGNAL dev wifi list | sed -rn 's/(.*[^ ])[ ]*(..:..:..:..:..:..)  ([0-9]*) .*/\&wifi=mac:\2|ssid:\1|ss:\3/p' | tr -d '\n')" | grep -Po '"l(at|ng)" : \K[0-9.,]+' | tr -d '\n'
 }
 wget "https://maps.google.com/maps/api/staticmap?maptype=satellite&center=$(whereami)&zoom=15&scale=2&size=640x360" -O ~/as_above_so_below.png
 #This line expands the image to a wallpaper size. Depends on imagemagick and is gonna be probably useless since you window manager is going to do that anyway.

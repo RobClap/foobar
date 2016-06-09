4c9498293449792048cdc37d644df6af67080a4e
RobClap <robclap8@gmail.com>
2016-06-10 00:31:53 +0200

    One liner. For real.

--- as_above_so_below/oneliner-lnx.sh
+++ as_above_so_below/oneliner-lnx.sh
@@ -1,5 +1,8 @@
-#depends on nmcli
-whereami () {
-curl "https://maps.googleapis.com/maps/api/browserlocation/json?browser=firefox&sensor=true" --data-urlencode "$(nmcli -f SSID,BSSID,SIGNAL dev wifi list | sed -rn 's/(.*[^ ])[ ]*(..:..:..:..:..:..)  ([0-9]*) .*/\&wifi=mac:\2|ssid:\1|ss:\3/p' | tr -d '\n')" | grep -Po '"l(at|ng)" : \K[0-9.,]+' | tr -d '\n'
-}
-wget "https://maps.google.com/maps/api/staticmap?maptype=satellite&center=$(whereami)&zoom=15&scale=2&size=640x360" -O ~/as_above_so_below.png
+#depends on nmcli, but it is one line
+wget "https://maps.google.com/maps/api/staticmap?maptype=satellite&center=$(
+curl "https://maps.googleapis.com/maps/api/browserlocation/json?browser=firefox&sensor=true" \
+	--data-urlencode "$(nmcli -f SSID,BSSID,SIGNAL dev wifi list | 
+		sed -rn 's/(.*[^ ])[ ]*(..:..:..:..:..:..)  ([0-9]*) .*/\&wifi=mac:\2|ssid:\1|ss:\3/p' | 
+		tr -d '\n')" | 
+	grep -Po '"l(at|ng)" : \K[0-9.,]+' | 
+	tr -d '\n')&zoom=15&scale=2&size=640x360" -O ~/as_above_so_below.png
